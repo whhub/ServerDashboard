364cdc861a25374974234023fc898c1a043c8e57
sam wang <hui.wang@agfa.com>
2021-06-03 10:07:37 +0800

    03 ng g c servers/server-list -m app

---
+++ src/app/servers/server-list/server-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sd-server-list',
+  templateUrl: './server-list.component.html',
+  styleUrls: ['./server-list.component.css']
+})
+export class ServerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
